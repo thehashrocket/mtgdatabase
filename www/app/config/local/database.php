edb693d0ae51a39f85bd2f04bb5e3e9838a31fa1
Jason Shultz <jasshultz@gmail.com>
2014-01-11 14:31:50 -0700

    setup db for local and hosting on mtgdatabase.com

--- www/app/config/local/database.php
+++ www/app/config/local/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'mtgdatab_magicdb',
-			'username'  => 'mtgdatab_magicu',
-			'password'  => 'I,s~g~k=f*Tk',
+			'database'  => 'mtgdatabase',
+			'username'  => 'root',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
